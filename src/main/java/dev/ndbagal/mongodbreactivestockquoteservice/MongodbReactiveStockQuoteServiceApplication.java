f00a9ab5998b90a4f9024d7eb4504af737f12bbe
Navnath Bagal <me@ndbagal.dev>
2021-04-03 18:22:57 +0530

    Initial commit

---
+++ src/main/java/dev/ndbagal/mongodbreactivestockquoteservice/MongodbReactiveStockQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.ndbagal.mongodbreactivestockquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbReactiveStockQuoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbReactiveStockQuoteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ndbagal/mongodbreactivestockquoteservice/MongodbReactiveStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ndbagal.mongodbreactivestockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbReactiveStockQuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
